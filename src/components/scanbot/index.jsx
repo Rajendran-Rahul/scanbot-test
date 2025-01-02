0a06f2f5b34f1bdc5f4252facbb8c9a78efb799d
unknown <mail2rahul95r@gmail.com>
2025-01-02 17:43:09 +0530

    autocapture and preview enabled

--- src/components/scanbot/index.jsx
+++ src/components/scanbot/index.jsx
@@ -3,133 +3,145 @@ import "./scanbot.css";
 import { useEffect, useState } from "react";
 
 const Scanbot = () => {
-    const DOCUMENT_SCANNER_CONTAINER = "document-scanner-view";
+  const DOCUMENT_SCANNER_CONTAINER = "document-scanner-view";
 
-    const LICENSE_KEY =
-        "hEH0Wbx1pZJdQXqRVrvGc0yiGpAEPX" +
-        "/2P8qVF0qND02w5r/e9VyIsXVItu/9" +
-        "E3b9caVgSNPpg4DAnE1rUn9v1SNLqW" +
-        "5fM/ripSXPMnkNUkoUdrYkI4rK/1lE" +
-        "XjQcjoxRu0BBoLKVuMGZ4tYe3kalp2" +
-        "eqMljYfdfMht4MScgMHLto33HwpLCK" +
-        "xkISTMYDnNevrY1Yjea0j55ktc/vRl" +
-        "pLrd1TMa49HbqGEkn/fhcVNY800el6" +
-        "7Cq51HzgnI2cElCL5kb0Vl9QdzT8KX" +
-        "bT+pBnDmvBuTWG7AXtUF+caXrL4UPH" +
-        "z0cG14HxAMNfvcjqJauliZYBANciZB" +
-        "9sDP7s3WIy/g==\nU2NhbmJvdFNESw" +
-        "psb2NhbGhvc3R8c2NhbmJvdC10ZXN0" +
-        "LnZlcmNlbC5hcHAKMTczNjI5NDM5OQ" +
-        "o4Mzg4NjA3Cjg=\n";
+  const LICENSE_KEY =
+    "hEH0Wbx1pZJdQXqRVrvGc0yiGpAEPX" +
+    "/2P8qVF0qND02w5r/e9VyIsXVItu/9" +
+    "E3b9caVgSNPpg4DAnE1rUn9v1SNLqW" +
+    "5fM/ripSXPMnkNUkoUdrYkI4rK/1lE" +
+    "XjQcjoxRu0BBoLKVuMGZ4tYe3kalp2" +
+    "eqMljYfdfMht4MScgMHLto33HwpLCK" +
+    "xkISTMYDnNevrY1Yjea0j55ktc/vRl" +
+    "pLrd1TMa49HbqGEkn/fhcVNY800el6" +
+    "7Cq51HzgnI2cElCL5kb0Vl9QdzT8KX" +
+    "bT+pBnDmvBuTWG7AXtUF+caXrL4UPH" +
+    "z0cG14HxAMNfvcjqJauliZYBANciZB" +
+    "9sDP7s3WIy/g==\nU2NhbmJvdFNESw" +
+    "psb2NhbGhvc3R8c2NhbmJvdC10ZXN0" +
+    "LnZlcmNlbC5hcHAKMTczNjI5NDM5OQ" +
+    "o4Mzg4NjA3Cjg=\n";
 
-    // Pass the license key to the Scanbot SDK to initialize it.
-    // Please refer to the corresponding setup guide in our documentation:
-    // https://docs.scanbot.io
+  // Pass the license key to the Scanbot SDK to initialize it.
+  // Please refer to the corresponding setup guide in our documentation:
+  // https://docs.scanbot.io
 
-    useEffect(() => {
-        const init = async () => {
-            await ScanbotSDK.initialize({
-                licenseKey: LICENSE_KEY,
-                engine: "wasm"
-            })
-        }
-        init();
-    }, [LICENSE_KEY])
+  useEffect(() => {
+    const initSDK = async () => {
+      try {
+        await ScanbotSDK.initialize({
+          licenseKey: LICENSE_KEY,
+          engine: "wasm",
+        });
+      } catch (error) {
+        console.error("Scanbot initialization error:", error);
+      }
+    };
+    initSDK();
+  }, [LICENSE_KEY]);
 
-    const [imgUrl, setImgUrl] = useState("")
+  const [imgUrl, setImgUrl] = useState("");
 
-    const scannerConfig = {
-        containerId: DOCUMENT_SCANNER_CONTAINER,
-        text: {
-            hint: {
-                OK: "Capturing your document...",
-                OK_SmallSize: "The document is too small. Try moving closer.",
-                OK_BadAngles:
-                    "This is a bad camera angle. Hold the device straight over the document.",
-                OK_BadAspectRatio:
-                    "Rotate the device sideways, so that the document fits better into the screen.",
-                OK_OffCenter: "Try holding the device at the center of the document.",
-                Error_NothingDetected:
-                    "Please hold the device over a document to start scanning.",
-                Error_Brightness: "It is too dark. Try turning on a light.",
-                Error_Noise: "Please move the document to a clear surface.",
-            },
+  const scannerConfig = {
+    containerId: DOCUMENT_SCANNER_CONTAINER,
+    text: {
+      hint: {
+        OK: "Capturing your document...",
+        OK_SmallSize: "The document is too small. Try moving closer.",
+        OK_BadAngles:
+          "This is a bad camera angle. Hold the device straight over the document.",
+        OK_BadAspectRatio:
+          "Rotate the device sideways, so that the document fits better into the screen.",
+        OK_OffCenter: "Try holding the device at the center of the document.",
+        Error_NothingDetected:
+          "Please hold the device over a document to start scanning.",
+        Error_Brightness: "It is too dark. Try turning on a light.",
+        Error_Noise: "Please move the document to a clear surface.",
+      },
+    },
+    style: {
+      outline: {
+        polygon: {
+          fillCapturing: "rgba(0, 255, 0, 0.2)",
+          strokeCapturing: "green",
+          fillSearching: "rgba(255, 0, 0, 0.2)",
+          strokeSearching: "red",
         },
-        style: {
-            outline: {
-                polygon: {
-                    fillCapturing: "rgba(0, 255, 0, 0.2)",
-                    strokeCapturing: "green",
-                    fillSearching: "rgba(255, 0, 0, 0.2)",
-                    strokeSearching: "red",
-                },
-                label: {
-                    position: "absolute",
-                    top: "90%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    textAlign: "center",
-                    backgroundColor: "rgba(0, 0, 0, 0.7)",
-                    color: "white",
-                    borderRadius: "0.25em",
-                    padding: "0.5em",
-                    fontFamily: "sans-serif",
-                    fontSize: "1em"
-                },
-                path: {
-                    stroke: "green",
-                    strokeWidth: 4,
-                }
-            },
-            captureButton: {
-                color: "white"
-            },
+        label: {
+          position: "absolute",
+          top: "90%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          color: "white",
+          borderRadius: "0.25em",
+          padding: "0.5em",
+          fontFamily: "sans-serif",
+          fontSize: "1em",
         },
-        preferredCamera: 'camera2 0, facing back',
-        onDocumentDetected: result => handleDocumentDetection(result)
+        path: {
+          stroke: "green",
+          strokeWidth: 4,
+        },
+      },
+      captureButton: {
+        color: "white",
+      },
+    },
+    preferredCamera: "camera2 0, facing back",
+    onDocumentDetected: (result) => handleDocumentDetection(result),
+  };
+
+  const scannerInstance = () =>
+    ScanbotSDK.instance.createDocumentScanner(scannerConfig);
+
+  const handleDocumentScanner = async () => {
+    try {
+      (await scannerInstance()).enableAutoCapture();
+    } catch (error) {
+      console.log("ERROR:", error);
     }
+  };
 
-    const scannerInstance = () => ScanbotSDK.instance.createDocumentScanner(scannerConfig)
-
-    const handleDocumentScanner = async () => {
-        try {
-            ((await scannerInstance()).enableAutoCapture())
-            setCount((prev) => prev+1)
-        } catch (error) {
-            console.log("ERROR:", error)
-        }
+  const handleDocumentDetection = async (result) => {
+    const { cropped } = result; //the value of cropped is of type Base64 image
+    const uint8Array = new Uint8Array(cropped);
+    const blob = new Blob([uint8Array], { type: "image/jpeg" });
+    const imageUrl = URL.createObjectURL(blob);
+    if (imageUrl) {
+      setImgUrl(imageUrl);
+      (await scannerInstance()).disableAutoCapture();
+      (await scannerInstance()).dispose();
     }
+  };
 
-    const handleDocumentDetection = async (result) => {
-        const { cropped } = result  //the value of cropped is of type Base64 image
-        const uint8Array = new Uint8Array(cropped);
-        const blob = new Blob([uint8Array], { type: 'image/jpeg' });
-        const imageUrl = URL.createObjectURL(blob);
-        if (imageUrl) {
-            (await scannerInstance()).disableAutoCapture()
-            setImgUrl(imageUrl)
-        }
-    }
+  return (
+    <div>
+      <h2>Document Scanner</h2>
 
-    return (
-        <div>
-            <h2>Document Scanner</h2>
+      <div
+        id={DOCUMENT_SCANNER_CONTAINER}
+        style={{ position: "relative", height: "100%", width: "100%" }}
+      >
+        <h3>Scanbot</h3>
+        <button onClick={() => handleDocumentScanner()}>
+          Open Document Scanner
+        </button>
+      </div>
 
-            <div id={DOCUMENT_SCANNER_CONTAINER} style={{ position: "relative", height: "100%", width: "100%" }}>
-                <h3>Scanbot</h3>
-                <button onClick={() => handleDocumentScanner()}>Click Me!!!</button>
-            </div>
-
-            {
-                imgUrl && <>
-                    <h2>Preview of Scanned Document</h2>
-                    <img src={imgUrl} alt="Scanned Document" style={{ maxWidth: '100%' }} />
-                </>
-            }
-        </div>
-
-    )
-}
-
+      {imgUrl && (
+        <>
+          <h2>Preview of Scanned Document</h2>
+          <img
+            src={imgUrl}
+            alt="Scanned Document"
+            style={{ maxWidth: "100%" }}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Scanbot;
