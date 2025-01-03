d24cabafdcffee4765c82c09b6eb936f7ce90180
unknown <mail2rahul95r@gmail.com>
2025-01-03 10:18:07 +0530

    moved constant values to constants file

--- src/components/scanbot/index.jsx
+++ src/components/scanbot/index.jsx
@@ -1,30 +1,10 @@
 import ScanbotSDK from "scanbot-web-sdk";
 import "./scanbot.css";
+import { DOCUMENT_SCANNER_CONTAINER, LICENSE_KEY } from "./constants";
 import { useEffect, useState } from "react";
 
 const Scanbot = () => {
-  const DOCUMENT_SCANNER_CONTAINER = "document-scanner-view";
-
-  const LICENSE_KEY =
-    "hEH0Wbx1pZJdQXqRVrvGc0yiGpAEPX" +
-    "/2P8qVF0qND02w5r/e9VyIsXVItu/9" +
-    "E3b9caVgSNPpg4DAnE1rUn9v1SNLqW" +
-    "5fM/ripSXPMnkNUkoUdrYkI4rK/1lE" +
-    "XjQcjoxRu0BBoLKVuMGZ4tYe3kalp2" +
-    "eqMljYfdfMht4MScgMHLto33HwpLCK" +
-    "xkISTMYDnNevrY1Yjea0j55ktc/vRl" +
-    "pLrd1TMa49HbqGEkn/fhcVNY800el6" +
-    "7Cq51HzgnI2cElCL5kb0Vl9QdzT8KX" +
-    "bT+pBnDmvBuTWG7AXtUF+caXrL4UPH" +
-    "z0cG14HxAMNfvcjqJauliZYBANciZB" +
-    "9sDP7s3WIy/g==\nU2NhbmJvdFNESw" +
-    "psb2NhbGhvc3R8c2NhbmJvdC10ZXN0" +
-    "LnZlcmNlbC5hcHAKMTczNjI5NDM5OQ" +
-    "o4Mzg4NjA3Cjg=\n";
-
-  // Pass the license key to the Scanbot SDK to initialize it.
-  // Please refer to the corresponding setup guide in our documentation:
-  // https://docs.scanbot.io
+  const [imgUrl, setImgUrl] = useState("");
 
   useEffect(() => {
     const initSDK = async () => {
@@ -39,8 +19,6 @@ const Scanbot = () => {
     };
     initSDK();
   }, [LICENSE_KEY]);
-
-  const [imgUrl, setImgUrl] = useState("");
 
   const scannerConfig = {
     containerId: DOCUMENT_SCANNER_CONTAINER,
@@ -105,15 +83,19 @@ const Scanbot = () => {
   };
 
   const handleDocumentDetection = async (result) => {
-    const { cropped } = result; //the value of cropped is of type Base64 image
+    const { cropped } = result; //the value of cropped is of type Uint8Array image
     const uint8Array = new Uint8Array(cropped);
     const blob = new Blob([uint8Array], { type: "image/jpeg" });
     const imageUrl = URL.createObjectURL(blob);
     if (imageUrl) {
       setImgUrl(imageUrl);
-      (await scannerInstance()).disableAutoCapture();
-      (await scannerInstance()).dispose();
+      handlePostDocumentCapture();
     }
+  };
+
+  const handlePostDocumentCapture = async () => {
+    (await scannerInstance()).disableAutoCapture();
+    (await scannerInstance())?.dispose();
   };
 
   return (
